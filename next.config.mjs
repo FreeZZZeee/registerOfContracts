f25dde4368640f4d6ceabcc86c60f78dbb759081
FreeZZZeee <amazing.leo@yandex.ru>
2024-03-26 15:59:25 +0500

    Исправление

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: 'https://testzakupki.pstu.ru/',
+                permanent: true,
+            },
+        ]
+    },
+};
 
 export default nextConfig;
